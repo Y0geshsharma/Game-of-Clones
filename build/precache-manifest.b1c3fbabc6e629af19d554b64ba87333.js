116a1509abe3de2589edeab871eb8459a31efe63
Yogesh Sharma <yogeshniit2016@gmail.com>
2019-10-13 20:16:19 +0530

    Add files via upload
    Build files

---
+++ build/precache-manifest.b1c3fbabc6e629af19d554b64ba87333.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299e971cf9acdb6da3438c8a73546233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f28998df1d0b216470",
+    "url": "/static/css/main.c38fc29a.chunk.css"
+  },
+  {
+    "revision": "d8c978f5c2204e7e96fa",
+    "url": "/static/js/2.5bcb0522.chunk.js"
+  },
+  {
+    "revision": "96f28998df1d0b216470",
+    "url": "/static/js/main.3a18b27e.chunk.js"
+  },
+  {
+    "revision": "99bd88328e2436066f51",
+    "url": "/static/js/runtime-main.5187d350.js"
+  },
+  {
+    "revision": "c722e2d1ddc0b6ace78f7428312e366f",
+    "url": "/static/media/41st_Elite_Corps.c722e2d1.png"
+  },
+  {
+    "revision": "71b7e4ae50e035c9962a5593bd0c087f",
+    "url": "/static/media/AAT_BF2.71b7e4ae.png"
+  },
+  {
+    "revision": "d749864628335a525e4bf90fd838dc6d",
+    "url": "/static/media/ARCTrooper-CotR.d7498646.png"
+  },
+  {
+    "revision": "ce0917e5619f52b93e6f317bd8a2d676",
+    "url": "/static/media/B2-RP-battle-droid-SWBF2.ce0917e5.png"
+  },
+  {
+    "revision": "708609bcd69d252deed4695773de005d",
+    "url": "/static/media/Battle_Droids.708609bc.png"
+  },
+  {
+    "revision": "3c8157b114509ee7bed5a20db2bd17c2",
+    "url": "/static/media/Christophsis_shield.3c8157b1.png"
+  },
+  {
+    "revision": "5fb4fd3e6d122645ead24fd02afada50",
+    "url": "/static/media/CloneJumpTrooper.5fb4fd3e.jpg"
+  },
+  {
+    "revision": "50fd1d1444b1ffd8905671e03f7da47d",
+    "url": "/static/media/CloneSCUBAtrooper-TCWs4BR1.50fd1d14.png"
+  },
+  {
+    "revision": "a656e953ebf6c1682bedd7b07ad89f4a",
+    "url": "/static/media/Clone_Commander_CotR.a656e953.png"
+  },
+  {
+    "revision": "023e652a41abb590c6c0a6dad4597326",
+    "url": "/static/media/Clone_heavy_gunner.023e652a.jpg"
+  },
+  {
+    "revision": "4ebd0dbf2ce0144bfa4d7461f645f651",
+    "url": "/static/media/Clone_trooper_armor.4ebd0dbf.png"
+  },
+  {
+    "revision": "31d5d71694e8a7b0bd79775126998495",
+    "url": "/static/media/Grand_army_formation.31d5d716.jpg"
+  },
+  {
+    "revision": "8886b03ab82f476071c8cef65baff5bb",
+    "url": "/static/media/KixFull-SWE.8886b03a.png"
+  },
+  {
+    "revision": "a40da4234d73d86d2aaf6feb3066fc46",
+    "url": "/static/media/Military_Complex.a40da423.jpg"
+  },
+  {
+    "revision": "3e81b1e34e4974390e7675ed87f4558a",
+    "url": "/static/media/Phase1Jumptrooper_BFII.3e81b1e3.png"
+  },
+  {
+    "revision": "b3542940b1e20314f727c10842539c32",
+    "url": "/static/media/PhaseICloneTrooper2-Fathead.b3542940.png"
+  },
+  {
+    "revision": "c849046ad12a7c077228d9ed9e59f737",
+    "url": "/static/media/RiotCloneTrooper-TCWs2BR2.c849046a.png"
+  },
+  {
+    "revision": "371a175f4d6c74babf29e58269b11025",
+    "url": "/static/media/SWBF2_ARC_Troopers.371a175f.png"
+  },
+  {
+    "revision": "304fa889335eb6e1e9f4832018a90992",
+    "url": "/static/media/SepAAT.304fa889.jpg"
+  },
+  {
+    "revision": "1c05a5eeced0560faad2f13402fb16fd",
+    "url": "/static/media/Squad_Clone_Trooper_Ordnance_Specialist.1c05a5ee.png"
+  },
+  {
+    "revision": "af4f11ba35479cb9cca5efc17473974b",
+    "url": "/static/media/SuperBattleDroidDetail-SWE.af4f11ba.png"
+  },
+  {
+    "revision": "763e2687be9e5dc1a5a4570ea2671da8",
+    "url": "/static/media/Training_Room_Kamino_AtG.763e2687.png"
+  },
+  {
+    "revision": "1cca7fa69e1c56c5ae1bd64d4db60810",
+    "url": "/static/media/b1battledroid.1cca7fa6.png"
+  },
+  {
+    "revision": "9a7fde396c8a00306c8e40df97b048e5",
+    "url": "/static/media/battle.9a7fde39.jpg"
+  },
+  {
+    "revision": "16536c2505b895c7d6b9c8c31932ead5",
+    "url": "/static/media/mainbackground.16536c25.png"
+  },
+  {
+    "revision": "43f196f94fd37e2610e496e38f08de7c",
+    "url": "/static/media/soliders.43f196f9.png"
+  }
+]);
